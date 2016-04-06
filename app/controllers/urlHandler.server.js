e145f63c65da17b30c6bfc4ba3f9addbf7059d21
Vadim Grigorov <chichavl@gmail.com>
2016-04-06 05:37:16 +0000

    Solution

---
+++ app/controllers/urlHandler.server.js
@@ -0,0 +1,90 @@
+'use strict';
+
+var validUrl = require('valid-url');
+var Url = require('../models/url.js');
+
+
+	function genId(len) {
+		var i;
+		var chars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+		var id = '';
+		for (i=0;i<len;i++) {
+			id += chars.charAt(Math.floor(Math.random() * chars.length));
+		}
+		return id;
+	}
+
+function ApiHandler () {
+
+	this.getOriginalUrl = function (req, res) {
+		Url
+			.findByIdAndUpdate(req.params.id, { $inc: { 'counter': 1 } }, {select:{_id:0,}}, function(err, result) {
+				if (err) { throw err; }
+				
+				if (result !== null) {
+					res.redirect(result.original_url);	
+				}
+				else {
+					res.json({error: "This url is not in database."});
+				}
+			}); 
+	};
+
+	this.addUrl = function (req, res, next) {
+		var fullUrl = req.params.protocol + '//' + req.params.url;
+		if (validUrl.isUri(fullUrl)) {
+		    //valid url
+		    	Url.findOneAndUpdate({"original_url": fullUrl}, 
+			{$setOnInsert: 
+				{
+					"_id": genId(6),
+					"original_url": fullUrl,
+					"counter": 0
+				}
+			} ,
+			{upsert: true, 'new': true},
+			function(err, result){
+				if (err) { 
+					if (err.name === 'MongoError' && err.code === 11000) {
+			        // Duplicate username
+			        // TODO Notify on key duplication - need to increment genId
+			        // for more combinations
+			        console.log("key duplication - need to increment genId for more combinations");
+			        res.status(500).send({ error: 'Internal server error, try once again. If error persist contact support' });
+			      }
+					else {
+						throw err;	
+					}
+				}
+				res.json({ "original_url":result.original_url, "short_url":req.protocol + '://' + req.get('host') + '/' + result._id});
+			});
+		  } else {
+		  	//url invalid
+		    res.json({error: "Wrong url format, make sure you have a valid protocol and real site."});
+		  }
+	
+		
+		// var url = new Url ({
+		// 	"_id": genId(6),
+		// 	"original_url": req.params.protocol + '//' + req.params.url
+		// });
+		
+		// url.save(function(err, result){
+		// 	if (err) { 
+		// 		if (err.name === 'MongoError' && err.code === 11000) {
+		//         // Duplicate username
+		//         // TODO Notify on key duplication - need to increment genId
+		//         // for more combinations
+		//         console.log("key duplication - need to increment genId for more combinations");
+		//         res.status(500).send({ error: 'Internal server error, try once again. If error persist contact support' });
+		//       }
+		// 		else {
+		// 			throw err;	
+		// 		}
+		// 	}
+		// 	res.json({ "original_url":result.original_url, "short_url":req.protocol + '://' + req.get('host') + '/' + result._id});
+		// });
+	};
+}
+
+module.exports = ApiHandler;
--- app/routes/index.js
+++ app/routes/index.js
@@ -1,57 +1,34 @@
 'use strict';
 
 var path = process.cwd();
-var ClickHandler = require(path + '/app/controllers/clickHandler.server.js');
+var ApiHandler = require(path + '/app/controllers/urlHandler.server.js');
 
-module.exports = function (app, passport) {
+module.exports = function (app) {
 
-	function isLoggedIn (req, res, next) {
-		if (req.isAuthenticated()) {
-			return next();
-		} else {
-			res.redirect('/login');
-		}
-	}
+	// function isLoggedIn (req, res, next) {
+	// 	if (req.isAuthenticated()) {
+	// 		return next();
+	// 	} else {
+	// 		res.redirect('/login');
+	// 	}
+	// }
 
-	var clickHandler = new ClickHandler();
+	var apiHandler = new ApiHandler();
 
 	app.route('/')
-		.get(isLoggedIn, function (req, res) {
+		.get(function (req, res) {
 			res.sendFile(path + '/public/index.html');
 		});
 
-	app.route('/login')
-		.get(function (req, res) {
-			res.sendFile(path + '/public/login.html');
+	app.route('/new/:protocol//:url')
+		.get(apiHandler.addUrl);
+		
+	//route is invalid should return error (no double slashes)
+	app.route('/new/:url')
+		.get(function(req, res) {
+		    res.json({error: "Wrong url format, make sure you have a valid protocol and real site."});
 		});
-
-	app.route('/logout')
-		.get(function (req, res) {
-			req.logout();
-			res.redirect('/login');
-		});
-
-	app.route('/profile')
-		.get(isLoggedIn, function (req, res) {
-			res.sendFile(path + '/public/profile.html');
-		});
-
-	app.route('/api/:id')
-		.get(isLoggedIn, function (req, res) {
-			res.json(req.user.github);
-		});
-
-	app.route('/auth/github')
-		.get(passport.authenticate('github'));
-
-	app.route('/auth/github/callback')
-		.get(passport.authenticate('github', {
-			successRedirect: '/',
-			failureRedirect: '/login'
-		}));
-
-	app.route('/api/:id/clicks')
-		.get(isLoggedIn, clickHandler.getClicks)
-		.post(isLoggedIn, clickHandler.addClick)
-		.delete(isLoggedIn, clickHandler.resetClicks);
+	
+	app.route('/:id')
+		.get(apiHandler.getOriginalUrl);
 };
